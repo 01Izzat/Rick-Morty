753e9561f236ccacfee2382825a6acced62663df
Izzatilla <108586467+01Izzat@users.noreply.github.com>
2023-04-03 05:45:21 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { Header } from './components/Header/Header';
+import { Characters } from './pages/Characters/Characters';
+import { CharacterInfo } from './pages/CharacterInfo/CharacterInfo';
+import { Route, Routes } from 'react-router-dom';
+import { ErrorPage } from './pages/ErrorPage/ErrorPage';
+import { Expisode } from './pages/Episode/Episode';
+import { Location } from './pages/Location/Location';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Characters />} />
+        <Route path='/characters/:id' element={<CharacterInfo />} />
+        <Route path='/episodes' element={<Expisode />} />
+        <Route path='/location' element={<Location />} />
+        <Route path='/*' element={<ErrorPage />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
